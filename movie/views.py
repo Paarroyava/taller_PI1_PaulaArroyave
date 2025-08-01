1eb13a33b8ecbcdd75d4bd2818b1a3e3f74682ab
Paula Andrea Arroyave <paulaandreaarroyave@MacBook-Air-de-Paula.local>
2025-08-01 00:16:07 -0500

    Segundo commit

--- movie/views.py
+++ movie/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie 
+
 # Create your views here.
 
 def home(request):
-     #return HttpResponse('<h1>Welcome to Home Page<h1>')
+    #return HttpResponse('<h1>Welcome to Home Page<h1>')
     #return render(request, 'home.html')
-    return render(request, 'home.html', {'name': 'Paula'})
+    return render(request, 'home.html', {'name': 'Paula Arroyave'})
+    searchTerm = request.GET.get('searchMovie')
+    movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
 
 def about(request):
-    return HttpResponse('<h1>Holis<h1>')
+    #return HttpResponse('<h1>Holis<h1>')
+    return render(request, 'about.html')
